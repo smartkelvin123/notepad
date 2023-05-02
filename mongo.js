2c504d5dba4f631fec35c6c858e4ccb61edbc622
smartkelvin123 <105611054+smartkelvin123@users.noreply.github.com>
2023-05-02 13:41:26 +0100

    files

---
+++ mongo.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://Smartkelvin:<password>@cluster0.xpjq0rw.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+const note = new Note({
+  content: "HTML is Easy",
+  important: true,
+});
+
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
