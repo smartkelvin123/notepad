8e1b34a5b0ff18c8ab899d2abb4ead988a485f06
smartkelvin123 <105611054+smartkelvin123@users.noreply.github.com>
2023-05-13 04:45:09 +0100

    fix

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const mongoose = require("mongoose");
 require("express-async-errors");
 
 const notesRouter = require("./controllers/notes");
-const usersRouter = require("./controllers/users");
+const usersRouter = require("./controllers/user");
+const loginRouter = require("./controllers/login");
+
 const middleware = require("./utils/middleware");
 
 mongoose.set("strictQuery", false);
@@ -30,6 +32,7 @@ app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
